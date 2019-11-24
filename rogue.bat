30d1025bde2fdb601d9d53b5049019876458b741
J-Tech-Foundation <jtechmaster02@gmail.com>
2019-11-24 02:51:37 +0000

    Update rogue.bat

--- rogue.bat
+++ rogue.bat
@@ -4,6 +4,12 @@ SET /A ROGUE_PATH=%PATH%
 SET /A BOOT=C:\bootmgr.bat
 
 
-if %1=="no-reboot" (
+if %1=="/no-reboot" (
     SET /A BOOT=""
+)
+if %1=="/developer-mode" (
+    @echo on
+)
+ELSE if %1=="/dm" (
+    @echo on
 )
