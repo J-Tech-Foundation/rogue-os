df2d9d5f72285b8c49287199941a65ee16795aa9
JT-Nexus Foundation <jtechmaster02@gmail.com>
2019-11-24 03:53:01 +0000

    make the code look pretty

--- rogue.bat
+++ rogue.bat
@@ -17,11 +17,13 @@ rem bootmgr.bat will be added by on of our first lovely developers to support us
 if exists %ROGUE_PATH%\bootmgr.bat (
     if not exists %BOOT% (
         rem check to make sure that the line below actually restarts the PC and runs the right commands
-        shutdown /f /s /t 20 /c "rogue is restarting the PC to continue installation"
-        goto boot
+        shutdown /f /s /t 20 /c "rogue is restarting the PC to continue installation" && goto boot
     )
 )
 
 :boot
 
-SET /A registry=%ROGUE_PATH%\registry
+SET /A registry=%ROGUE_PATH%\registry.bat
+call %registry% /config BASH
+
+
