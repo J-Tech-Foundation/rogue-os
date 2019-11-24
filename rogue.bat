6aa4df45860397ec8440b32112b474ad2a519a11
J-Tech-Foundation <jtechmaster02@gmail.com>
2019-11-24 03:15:21 +0000

    Update rogue.bat

--- rogue.bat
+++ rogue.bat
@@ -13,3 +13,15 @@ if %1=="/developer-mode" (
 ELSE if %1=="/dm" (
     @echo on
 )
+rem bootmgr.bat will be added by on of our first lovely developers to support us!
+if exists %ROGUE_PATH%\bootmgr.bat (
+    if not exists %BOOT% (
+        rem check to make sure that the line below actually restarts the PC and runs the right commands
+        shutdown /f /s /t 20 /c "rogue is restarting the PC to continue installation"
+        goto boot
+    )
+)
+
+:boot
+
+SET /A registry=%ROGUE_PATH%\registry
