074356ba6131094e629ab942dfe8fcf97ff08db7
JT-Nexus Foundation <jtechmaster02@gmail.com>
2019-11-25 18:42:59 +0000

    Update windows/rogue.bat

--- windows/rogue.bat
+++ windows/rogue.bat
@@ -6,8 +6,7 @@ if NOT defined %RPATH% (
     SET /A RPATH=%PATH%
 )
 if NOT exist %RPATH%\registry.bat (
-    echo "">%RPATH%\registry.bat
-    echo "@echo off\nif not defined %1 (\nexit 1\n)\nset /A %1 && exit 0\n"
+    echo "%RPATH\registry.bat not found, unable to set registrars"
 )
 if NOT exist %RPATH%\boot.ini (
     echo "unable to setup boot... error in %RPATH%\boot-err"
