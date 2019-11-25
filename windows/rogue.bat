cb8624f1431628b6018e26d5bc09bb98bd8ba73e
JT-Nexus Foundation <jtechmaster02@gmail.com>
2019-11-25 20:25:02 +0000

    Update windows/rogue.bat

--- windows/rogue.bat
+++ windows/rogue.bat
@@ -5,10 +5,18 @@ rem -----
 if NOT defined %RPATH% (
     SET /A RPATH=%PATH%
 )
+rem this will be removed soon, does not even work 
 if NOT exist %RPATH%\registry.bat (
-    echo "%RPATH\registry.bat not found, unable to set registrars"
+    echo "%RPATH\registry.bat not found, unable to set registrars... error in %RPATH%\boot-err" 
+    echo "%RPATH%\registry.bat not found, unable to set variables" > %RPATH%\boot-err && exit 0
 )
+rem this will be handled by jt nexus
 if NOT exist %RPATH%\boot.ini (
     echo "unable to setup boot... error in %RPATH%\boot-err"
     echo "%RPATH%\boot.ini not found, unable to write boot record" > %RPATH%\boot-err && exit 0
+)
+rem we need designs and code for the os, submit your code andboot design for rgboot.exe
+if NOT exist %RPATH%\rgboot.exe (
+    echo "unable to setup boot... error in %RPATH%\boot-err"
+    echo "%RPATH%\rgboot.exe not found, unable to set master boot record" > %ROATH%\boot.err && exit 0
 )
