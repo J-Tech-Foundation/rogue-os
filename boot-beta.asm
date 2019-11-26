95fe24616e24edf8737eaefb641db8de001a4c0d
JT-Nexus Foundation <jtechmaster02@gmail.com>
2019-11-26 16:27:13 +0000

    Add new file

---
+++ boot-beta.asm
@@ -0,0 +1,14 @@
+section	.text
+   global _start     
+	
+_start:	            
+section	.data
+; mov edx,LENGTH
+; mov ecx,MSG
+; mov ebx,1
+; mov eax,4
+; int 0x80
+
+; end file:
+; move eax,1
+; int 0x80
