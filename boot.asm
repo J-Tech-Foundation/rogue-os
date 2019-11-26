ee6fe996b49e3e2a800bf071e4b89a1ddd622628
JT-Nexus Foundation <jtechmaster02@gmail.com>
2019-11-26 16:13:41 +0000

    Update boot.asm

--- boot.asm
+++ boot.asm
@@ -2,13 +2,7 @@ section	.text
    global _start     
 	
 _start:	            
-   mov	edx,len     
-   mov	ecx,msg
-   mov	ebx,1       
-   mov	eax,4       
-   int	0x80        
-   mov	eax,1       
-   int	0x80        
+; we need some help!    
 section	.data
 
 ; template:
