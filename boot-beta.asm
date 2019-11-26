3e8560db7d46e38743d2b7e8cd7e08c9ddad2f62
JT-Nexus Foundation <jtechmaster02@gmail.com>
2019-11-26 18:09:32 +0000

    Update boot-beta.asm

--- boot-beta.asm
+++ boot-beta.asm
@@ -1,8 +1,21 @@
 section	.text
    global _start     
 	
-_start:	            
+_start:	     
+    mov edx,vimlen
+    mov ecx,vim
+    mov ebx,1
+    mov eax,4
+    int 0x80
+    
+    mov edx,4
+    mov ecx,"yeet"
+    mov ebx,1
+    mov eax,4
+    int 0x80
 section	.data
+    vim db 'loading',0xa
+    vimlen equ $ - vim
 ; mov edx,LENGTH
 ; mov ecx,MSG
 ; mov ebx,1
@@ -12,7 +25,6 @@ section	.data
 ; end file:
 ; move eax,1
 ; int 0x80
-
 
 ;string setup:
 ;VAR_NAME db 'MSG_HERE',0xa
