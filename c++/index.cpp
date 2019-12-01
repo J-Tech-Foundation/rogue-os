15b7d1b1c4e18ae5c6f4565fe7aad194596c4187
JT-Nexus Foundation <jtechmaster02@gmail.com>
2019-12-01 02:03:41 +0000

    Add new file

---
+++ c++/index.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+int main()
+{
+  std::cout << "test";
+  return 0;
+}
+//registry<4LETTERPIN> variable_save
+//         ^^^^^^^^^^ --> is login pin for your rogue operating system
+
+
+template<char v4 [4]>
+class registry{
+  private:
+  char bit8 [16];
+  public:
+  void set (value = bit8) {
+    if (value==bit8){
+      return 0
+    };
+  };
+};
